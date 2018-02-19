ec0f55d1f9069db80eeb0abde7d837ee124332f0
Aleš Menzel <alesmenzel@gmail.com>
2018-02-19 01:18:37 +0100

    test: enable parallel test with no limit

--- src/parallel.spec.js
+++ src/parallel.spec.js
@@ -4,44 +4,44 @@ const asyncFunction = require('./helper/pointless-async');
 const parallel = require('./parallel');
 
 describe('parallel', () => {
-  // it('runs tasks in parallel for iterable', async () => {
-  //   const tasks = {
-  //     * [Symbol.iterator]() {
-  //       yield () => asyncFunction('first', 5);
-  //       yield () => asyncFunction('second', 3);
-  //       yield () => asyncFunction('third', 1);
-  //     },
-  //   };
+  it('runs tasks in parallel for iterable', async () => {
+    const tasks = {
+      * [Symbol.iterator]() {
+        yield () => asyncFunction('first', 5);
+        yield () => asyncFunction('second', 3);
+        yield () => asyncFunction('third', 1);
+      },
+    };
 
-  //   const res = await parallel(tasks);
-  //   expect(res).to.deep.equal(['first', 'second', 'third']);
-  // });
+    const res = await parallel(tasks);
+    expect(res).to.deep.equal(['first', 'second', 'third']);
+  });
 
-  // it('runs tasks in parallel for array', async () => {
-  //   const tasks = [
-  //     () => asyncFunction('first', 5),
-  //     () => asyncFunction('second', 3),
-  //     () => asyncFunction('third', 1),
-  //   ];
+  it('runs tasks in parallel for array', async () => {
+    const tasks = [
+      () => asyncFunction('first', 5),
+      () => asyncFunction('second', 3),
+      () => asyncFunction('third', 1),
+    ];
 
-  //   const res = await parallel(tasks);
-  //   expect(res).to.deep.equal(['first', 'second', 'third']);
-  // });
+    const res = await parallel(tasks);
+    expect(res).to.deep.equal(['first', 'second', 'third']);
+  });
 
-  // it('runs tasks in parallel for object', async () => {
-  //   const tasks = {
-  //     first: () => asyncFunction('first', 5),
-  //     second: () => asyncFunction('second', 3),
-  //     third: () => asyncFunction('third', 1),
-  //   };
+  it('runs tasks in parallel for object', async () => {
+    const tasks = {
+      first: () => asyncFunction('first', 5),
+      second: () => asyncFunction('second', 3),
+      third: () => asyncFunction('third', 1),
+    };
 
-  //   const res = await parallel(tasks);
-  //   expect(res).to.deep.equal({
-  //     first: 'first',
-  //     second: 'second',
-  //     third: 'third',
-  //   });
-  // });
+    const res = await parallel(tasks);
+    expect(res).to.deep.equal({
+      first: 'first',
+      second: 'second',
+      third: 'third',
+    });
+  });
 
   it('runs limited tasks in parallel for iterable', async () => {
     const tasks = {
